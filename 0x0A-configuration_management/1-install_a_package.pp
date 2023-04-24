13165e25acc5fe995abefab42b32c74c1479250f
Arafat Motolani <Mhyzdamilola@gmail.com>
2023-04-24 08:21:45 -0700

    tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# install flask from pip3 using Puppet
+# Installs flask from pip3, version 2.1.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install-flask':
+  command => 'install Flask==2.1.0',
+  require => Package['python3-pip'],
 }
